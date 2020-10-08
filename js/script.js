f3c0c38466ca0f42d7490df8e6eb432999a2deff
Lena Korobova <lenakor45@gmail.com>
2020-10-08 13:27:07 +0500

    addaptation end

--- js/script.js
+++ js/script.js
@@ -1,7 +1,56 @@
-function btnclick()
-{
-    let age = document.getElementById('age').value;
-    if(age<=18)
-    console.log('stop');
-    else console.log('start');
-}
+$(document).on('click', '.send', function() {
+    var validateForm = $(document).find('#contacsform');
+    validateForm.validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            name: {
+                required: "Введите Ваш Имя",
+                minlength: "Имя должно быть более 2-х символов"
+            },
+            phone: {
+                required: "Введите Email",
+                minlength: "Поле должно быть более 5-ти символов",
+                email: "Некорректно введен Email"
+            }
+        },
+        submitHandler: function(form) {
+            form.submit();
+        },
+        errorPlacement: function(error, element) {
+            var item = element.parents('.item');
+            item.append(error);
+        }
+    });
+
+    validateForm.submit();
+});
+jQuery.validator.addMethod("accept", function(value, element, param) {
+    return value.match(new RegExp("." + param + "$"));
+});
+
+validateForm.validate({
+    rules: {
+        firstname: {
+            required: true,
+            accept: "accept: "[а - яА - ЯёЁa - zA - Z] + ",",
+            minlength: 2
+        }
+    },
+    messages: {
+        firstname: {
+            required: "Введите имя",
+            accept: "Разрешено вводить только буквы",
+            minlength: "Имя должно быть более 2-х символов"
+        }
+    }
+});
